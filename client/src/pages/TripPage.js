935862f143243af090215209b7f03d86c683de82
ZaharKaras <zahar.karas@gmail.com>
2023-12-20 23:33:45 +0300

    add routing

---
+++ client/src/pages/TripPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TripPage = () => {
+    return (
+        <div>
+            TripPage
+        </div>
+    );
+};
+
+export default TripPage;
---
+++ client/src/utils/consts.js
@@ -0,0 +1,6 @@
+export const ADMIN_ROUTE = '/admin'
+export const LOGIN_ROUTE = '/login'
+export const REGISTRATION_ROUTE = '/registration'
+export const AGENCY_ROUTE = '/'
+export const BASKET_ROUTE = '/basket'
+export const TRIP_ROUTE = '/trip'
